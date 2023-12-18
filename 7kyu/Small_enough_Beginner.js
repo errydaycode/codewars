cd45bb2ee71cd2c8d16448cf402a71008ea438fc
listopadgeorgii <listopadiogl@gmail.com>
2023-12-18 23:51:33 +0300

    Small enough? - Beginner

---
+++ 7kyu/Small_enough_Beginner.js
@@ -0,0 +1,15 @@
+// You will be given an array and a limit value.
+//     You must check that all values in the array are below or equal to the limit value.
+//     If they are, return true. Else, return false.
+//
+//     You can assume all values in the array are numbers.
+
+
+// first solution
+function smallEnough(a, limit){
+    let arr = a.filter(el => el <= limit)
+    return arr.length === a.length ? true : false
+}
+
+
+// refactored solution
