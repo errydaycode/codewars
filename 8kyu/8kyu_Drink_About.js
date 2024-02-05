490d4776ab11d31371fed9c6e33982ec74ccd747
listopadgeorgii <listopadiogl@gmail.com>
2024-02-05 23:47:57 +0300

    peopleWithAgeDrink

---
+++ 8kyu/8kyu_Drink_About.js
@@ -0,0 +1,14 @@
+function peopleWithAgeDrink(old) {
+    switch (true) {
+        case (old < 14):
+            return "drink toddy";
+        case (old < 18):
+            return "drink coke";
+        case (old < 21):
+            return "drink beer";
+        default:
+            return "drink whisky";
+    }
+}
+
+ console.log( peopleWithAgeDrink(100))
